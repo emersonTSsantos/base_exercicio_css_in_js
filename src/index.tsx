3af08abd628b037879115690ac1a4f30c9668959
Emerson Teixeira <emerson@exemplo.com>
2024-04-29 17:06:41 -0300

    resolvendo problema de importação

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
-import styles from './Cabecalho.module.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
 
-export default Cabecalho
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
